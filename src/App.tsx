a4e032f6c125746fdc0d5ce263d594746883b901
Jubair <mjahmed.wd@gmail.com>
2022-07-03 17:22:04 +0600

    initial components are made

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,41 @@
 import { useState } from 'react'
 import logo from './logo.svg'
+import {
+  BrowserRouter as Router,
+  Navigate,
+  Route,
+  Routes,
+} from "react-router-dom";
 import './App.css'
+import NewLogin from './modules/login/login';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      <Router>
+        {/* body */}
+        <div>
+          <div className="body-inner">
+            <Routes>
+              {/* {routingList?.map((item) => (
+                <Route
+                  path={item?.path}
+                  element={
+                    <Suspense fallback={<>..</>}>{item?.component}</Suspense>
+                  }
+                />
+              ))} */}
+              {/* <Route exact path="/demo" element={<Demo />} /> */}
+              <Route path="/" element={<NewLogin/>} />
+              {/* <Route path="/accessDenied" element={<AccessDeniedPage />} /> */}
+              <Route path="*" element={<h1>Error</h1>} />
+
+            </Routes>
+          </div>
+        </div>
+      </Router>
     </div>
   )
 }
