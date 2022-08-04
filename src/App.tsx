943b8d7a0b2d24fe4d4eafcae1931f9dc2188549
Jubair <mjahmed.wd@gmail.com>
2022-08-04 09:42:27 +0600

    form checked

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
+    <div className="">
       <Router>
         {/* body */}
         <div>
--- src/common/components/InputField/Input.tsx
+++ src/common/components/InputField/Input.tsx
@@ -1,5 +1,6 @@
-import { useFormikContext } from "formik";
+import { Field, useFormikContext } from "formik";
 import React, { FC } from "react";
+import FormikError from "./FormikError";
 import "./Input.scss";
 
 interface IInputProps {
@@ -9,20 +10,24 @@ interface IInputProps {
 
 const Input: FC<IInputProps> = ({ name, ...rest }) => {
 
-    const { values, setFieldValue }: {
+    const { values, setFieldValue, errors, touched }: {
         values: any;
         setFieldValue: (name: string, value: any) => void;
+        errors: any;
+        touched: any;
     } = useFormikContext();
 
     return (
-        <>
-            <input
+        <div>
+            <h5 style={{padding: '5px 0'}}>{rest?.label || name} :</h5>
+            <Field
                 value={values[name]}
                 name={name}
                 placeholder={`Enter ${rest?.label || name}`}
                 {...rest}
             />
-        </>
+            <FormikError errors={errors} touched={touched} name={name} />
+        </div>
     );
 };
 
